9149cdb9a50f4e92ec885f9bb9e7cf39a2687968
Hamg69 <muhammad17gb@gmail.com>
2021-03-16 16:03:00 +0000

    build

---
+++ build/precache-manifest.f68f785e17076623b3d878354ad5b1aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a01552e7526cfecd047acd5b2a71ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a934a85c00be0fa7efb1",
+    "url": "/static/css/main.93b0f790.chunk.css"
+  },
+  {
+    "revision": "8115369f8158f69388ba",
+    "url": "/static/js/2.76a40689.chunk.js"
+  },
+  {
+    "revision": "df34426e3c67cba54df30dae4655a86f",
+    "url": "/static/js/2.76a40689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a934a85c00be0fa7efb1",
+    "url": "/static/js/main.fd376d1c.chunk.js"
+  },
+  {
+    "revision": "ff0e68b34eebd701fc7d",
+    "url": "/static/js/runtime-main.6aa59f0c.js"
+  }
+]);
